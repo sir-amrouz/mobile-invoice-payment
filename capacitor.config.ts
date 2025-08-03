411c89c2275d14ab3247d2041f786cfaeb70e734
Sir Amrouz <amar.amrouz@gmail.com>
2025-08-03 13:16:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-invoice-payment',
+  webDir: 'www'
+};
+
+export default config;
